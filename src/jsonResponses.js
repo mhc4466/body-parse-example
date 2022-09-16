7f8e4b022ebb7d782067b86e8426ccdd34fa9b5a
Michael Chan (RIT Student) <mhc4466@rit.edu>
2022-09-16 13:50:38 -0400

    Finished class demo

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -24,7 +24,31 @@ const getUsers = (request, response) => {
 };
 
 const addUser = (request, response, body) => {
+  const responseJSON = {
+    message: 'Name and age are both required'
+  };
 
+  if (!body.name || !body.age) {
+    responseJSON.id = 'addUserMissingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  let responseCode = 204;
+
+  if (!users[body.name]) {
+    responseCode = 201;
+    users[body.name] = {};
+    users[body.name].name = body.name;
+  }
+
+  users[body.name].age = body.age;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created successfully.';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+
+  return respondJSONMeta(request, response, responseCode);
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -7,8 +7,31 @@ const jsonHandler = require('./jsonResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const handlePost = (request, response, parsedUrl) => {
-
+  if (parsedUrl.pathname === '/addUser') {
+    return parseBody(request, response, jsonHandler.addUser);
+  }
 };
+
+const parseBody = (request, response, handler) => {
+  const body = [];
+
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+
+    return handler(request, response, bodyParams);
+  });
+}
 
 const handleGet = (request, response, parsedUrl) => {
   if (parsedUrl.pathname === '/style.css') {
@@ -23,7 +46,12 @@ const handleGet = (request, response, parsedUrl) => {
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
 
-
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  }
+  else {
+    handleGet(request, response, parsedUrl);
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {
